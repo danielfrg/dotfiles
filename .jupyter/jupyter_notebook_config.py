ab2bcf188ff99bfd24487e89820e3469bb607df5
Daniel Rodriguez <df.rodriguez143@gmail.com>
2018-10-16 15:40:53 -0500

    Update

---
+++ .jupyter/jupyter_notebook_config.py
@@ -0,0 +1,73 @@
+c = get_config()
+
+# The IP address the notebook server will listen on.
+c.NotebookApp.ip = "0.0.0.0"
+
+## Set the Access-Control-Allow-Origin header
+#
+#  Use "*" to allow any origin to access your server.
+#
+#  Takes precedence over allow_origin_pat.
+c.NotebookApp.allow_origin = "*"
+
+## Specify what command to use to invoke a web browser when opening the notebook.
+#  If not specified, the default browser will be determined by the `webbrowser`
+#  standard library module, which allows setting of the BROWSER environment
+#  variable to override it.
+c.NotebookApp.open_browser = False
+c.NotebookApp.tornado_settings = {"debug": True}
+
+c.LabApp.token = ""
+
+# Tornado server settings
+c.NotebookApp.tornado_settings = {
+    "headers": {
+        "Content-Security-Policy": "frame-ancestors *"  # Allows app to be iframed
+    },
+    "debug": True  # Auto restart
+}
+
+#######################################
+# s3contents stuff
+#######################################
+
+# from s3contents import S3ContentsManager
+
+# c.NotebookApp.contents_manager_class = S3ContentsManager
+# c.S3ContentsManager.access_key_id = "access-key"
+# c.S3ContentsManager.secret_access_key = "secret-key"
+# c.S3ContentsManager.endpoint_url = "http://localhost:9000"
+# c.S3ContentsManager.bucket = "notebooks"
+# c.S3ContentsManager.prefix = "notebooks/test"
+
+## s3contents with HybridContentsManager
+# from s3contents import S3ContentsManager
+# from pgcontents.hybridmanager import HybridContentsManager
+# from IPython.html.services.contents.filemanager import FileContentsManager
+
+# c = get_config()
+
+# c.NotebookApp.contents_manager_class = HybridContentsManager
+
+# c.HybridContentsManager.manager_classes = {
+#     # Associate the root directory with a PostgresContentsManager.
+#     # This manager will receive all requests that don"t fall under any of the
+#     # other managers.
+#     "": S3ContentsManager,
+#     # Associate /directory with a FileContentsManager.
+#     "local_directory": FileContentsManager,
+# }
+
+# c.HybridContentsManager.manager_kwargs = {
+#     # Args for root PostgresContentsManager.
+#     "": {
+#         "access_key_id": "access-key",
+#         "secret_access_key": "secret-key",
+#         "endpoint_url": "http://localhost:9000",
+#         "bucket": "notebooks",
+#     },
+#     # Args for the FileContentsManager mapped to /directory
+#     "local_directory": {
+#         "root_dir": "/Users/drodriguez/Downloads",
+#     },
+# }
--- Makefile
+++ Makefile
@@ -1,63 +1,69 @@
 install: xcode homebrew powerline
 
-xcode:
+xcode: ## Install xcode
 	xcode-select --install
 .PHONY:xcode
 
-homebrew:
+homebrew:  ## Install homebrew
 	/usr/bin/ruby -e "$$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 .PHONY: homebrew
 
-powerline:
+powerline:  ##
 	@curl https://github.com/justjanne/powerline-go/releases/download/v1.11.0/powerline-go-darwin-amd64 -o /usr/local/bin/powerline-go -L
 	@chmod +x /usr/local/bin/powerline-go
 
-link: brew python zsh git tmux sshrc vscode
-
-brew:  ## brew
+brew:  ##
 	brew bundle
 .PHONY: brew
 
-python:  ## python
+link: python zsh git tmux sshrc vscode jupyter  ## Create symlinks to the all the stuff
+
+python:  ##
 	ln -sF $(CURDIR)/.pylintrc ~/.pylintrc; \
 	ln -sF $(CURDIR)/.condarc ~/.condarc
 .PHONY: python
 
-zsh:  ## zsh
+jupyter:
+	@mkdir -p ~/.jupyter/
+	ln -sf $(CURDIR)/.jupyter/jupyter_notebook_config.py ~/.jupyter/jupyter_notebook_config.py
+.PHONY: jupyter
+
+
+zsh:  ##
 	bash -c "$$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"; \
 	ln -sf $(CURDIR)/.zshrc ~/.zshrc
 .PHONY: zsh
 
-git:  ## git
+git:  ##
 	ln -sf $(CURDIR)/.gitconfig ~/.gitconfig; \
 	ln -sf $(CURDIR)/.gitignore_global ~/.gitignore_global
 .PHONY: zsh
 
-tmux:  ## tmux
+tmux:  ##
 	ln -sf $(CURDIR)/.tmux.conf ~/.tmux.conf
 .PHONY: tmux
 
-sshrc:  ## sshrc
+sshrc:  ##
 	ln -sf $(CURDIR)/.sshrc ~/.sshrc; \
 	ln -sf $(CURDIR)/.sshrc.d ~/.sshrc.d
 .PHONY: sshrc
 
-vscode:  ## vscode
+vscode:  ##
 	bash vscode/extensions.sh
 	ln -sf $(CURDIR)/vscode/settings.json ~/Library/Application\ Support/Code/User/settings.json
 	ln -sf $(CURDIR)/vscode/keybindings.json ~/Library/Application\ Support/Code/User/keybindings.json
 	ln -sf $(CURDIR)/vscode/snippets/ ~/Library/Application\ Support/Code/User/snippets
 .PHONY: vscode
 
-vi:  ## vi
+vi:  ##
 	ln -sF $(CURDIR)/.vim ~/.vim; \
 	ln -sf $(CURDIR)/.vimrc ~/.vimrc; \
 	git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim; \
 	vim +PluginInstall +qall
 .PHONY: vi
 
-gpg:  # gpg
+gpg:  ##
 	ln -sF ~/Google\ Drive/apps/.gnupg/ ~/.gnupg
 
 help:
-	@grep -E '^[a-zA-Z_-]+:.*?## .*$$' $(MAKEFILE_LIST) | sort | awk 'BEGIN {FS = ":.*?## "}; {printf "\033[36m%-30s\033[0m %s\n", $$1, $$2}'
+	@grep -E '^[a-zA-Z_-]+:.*?##.*$$' $(MAKEFILE_LIST) | sort | awk 'BEGIN {FS = ":.*?##"; OFS="\t\t"}; {printf "\033[36m%-30s\033[0m %s\n", $$1, ($$2==""?"":$$2)}'
