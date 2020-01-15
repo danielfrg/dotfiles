99495e7b776476f3b0f0c7f3d80a9dfcaedec6a1
Daniel Rodriguez <df.rodriguez143@gmail.com>
2020-01-15 10:18:25 -0500

    Update

--- install-fonts.sh
+++ install-fonts.sh
@@ -3,30 +3,30 @@
 # echo $0
 
 # Set source and target directories
-powerline_fonts_dir="$(pwd)/fonts"
+source_font_dir="$(pwd)/fonts"
 
-echo "Installing fonts from: " $powerline_fonts_dir
+echo "Installing fonts from: " $source_font_dir
 
 # if an argument is given it is used to select which fonts to install
 prefix="$1"
 
 if test "$(uname)" = "Darwin" ; then
   # MacOS
-  font_dir="$HOME/Library/Fonts"
+  target_font_dir="$HOME/Library/Fonts"
 else
   # Linux
-  font_dir="$HOME/.local/share/fonts"
-  mkdir -p $font_dir
+  target_font_dir="$HOME/.local/share/fonts"
+  mkdir -p $target_font_dir
 fi
 
 # Copy all fonts to user fonts directory
 echo "Copying fonts..."
-find "$powerline_fonts_dir" \( -name "$prefix*.[ot]tf" -or -name "$prefix*.pcf.gz" \) -type f -print0 | xargs -0 -n1 -I % cp "%" "$font_dir/"
+find "$source_font_dir" \( -name "$prefix*.[ot]tf" -or -name "$prefix*.pcf.gz" \) -type f -print0 | xargs -0 -n1 -I % cp "%" "$target_font_dir/"
 
 # Reset font cache on Linux
 if which fc-cache >/dev/null 2>&1 ; then
     echo "Resetting font cache, this may take a moment..."
-    fc-cache -f "$font_dir"
+    fc-cache -f "$target_font_dir"
 fi
 
-echo "Powerline fonts installed to $font_dir"
+echo "Fonts installed to $target_font_dir"
