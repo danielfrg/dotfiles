920663fa11fd6e55cb2ca75c6e1e3ff6bb790462
Daniel Rodriguez <daniel@danielfrg.com>
2020-05-27 15:25:55 -0500

    Update

--- zsh_custom/plugins/pythondev/pythondev.plugin.zsh
+++ zsh_custom/plugins/pythondev/pythondev.plugin.zsh
@@ -19,6 +19,7 @@ function pyclean() {
     find . -type f -name '*.py[co]' -delete
 	find . -type d -name __pycache__ -exec rm -rf {} +
 	find . -type d -name .ipynb_checkpoints -exec rm -rf {} +
+	find . -type d -name .pytest_cache -exec rm -rf {} +
 }
 
 
