93c9f3bae846a4bab3ab64f235f71d25a64277a5
Daniel Rodriguez <daniel@danielfrg.com>
2021-03-12 14:17:45 -0500

    Update

--- zsh_custom/plugins/base/base.plugin.zsh
+++ zsh_custom/plugins/base/base.plugin.zsh
@@ -184,7 +184,7 @@ alias sha256sum="shasum -a 256"
 
 # Helpers
 exportpathhere() { export PATH=$(pwd):$PATH }
-whoseport() { lsof -i ":$1" | grep LISTEN }
+port_listening_who() { lsof -i ":$1" | grep LISTEN }
 alias docker-transmission="open http://localhost:9091 && docker run -it -v ~/Downloads:/downloads -p 9091:9091 linuxserver/transmission"
 
 # Create a new directory and enter it
