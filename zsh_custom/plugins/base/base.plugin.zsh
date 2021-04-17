2c55dfae35c4d3b724179c4044214568a6f37bc5
Daniel Rodriguez <daniel@danielfrg.com>
2021-04-16 21:59:49 -0500

    Update

--- zsh_custom/plugins/base/base.plugin.zsh
+++ zsh_custom/plugins/base/base.plugin.zsh
@@ -173,7 +173,7 @@ done
 alias emptytrash="sudo rm -rfv /Volumes/*/.Trashes; sudo rm -rfv ~/.Trash; sudo rm -rfv /private/var/log/asl/*.asl"
 
 # Replacements
-alias cat='bat'
+alias cat='bat --style="header"'
 alias ccat='/bin/cat'
 alias ping='prettyping --nolegend'
 alias top='htop'
