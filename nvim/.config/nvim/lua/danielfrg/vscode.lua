e31a3b5dac7277495807ed3595c88b9ffd99c160
Daniel Rodriguez <daniel@danielfrg.com>
2023-04-12 00:53:39 -0500

    feat: More NeoVim VSCode

--- nvim/.config/nvim/lua/danielfrg/vscode.lua
+++ nvim/.config/nvim/lua/danielfrg/vscode.lua
@@ -10,12 +10,19 @@ end
 
 local opts = { silent = true }
 
+-- Keymaps
+keymap('n', '<Leader>bb', notify 'extension.goto-previous-buffer', opts)
+keymap('n', '<Leader>`', notify 'extension.goto-previous-buffer', opts)
+keymap('n', '<S-x>', notify 'extension.goto-previous-buffer', opts)
+
 -- LSP related keymaps
-keymap('n', '<Leader>gr', notify 'references-view.findReferences', opts) -- language references
-keymap('n', '<Leader>xd', notify 'workbench.actions.view.problems', opts) -- language diagnostics
+keymap('n', 'K', notify 'editor.action.showHover', opts)
+keymap('n', '<Leader>gd', notify 'editor.action.revealDefinition', opts)
+keymap('n', '<Leader>gD', notify 'editor.action.revealDeclaration', opts)
+keymap('n', '<Leader>gr', notify 'references-view.findReferences', opts)
 keymap('n', '<Leader>cf', notify 'editor.action.formatDocument', opts)
 keymap('n', '<Leader>cr', notify 'editor.action.rename', opts)
-keymap('n', '<Leader>xd', notify 'workbench.actions.view.problems', opts) -- language diagnostics
+keymap('n', '<Leader>xd', notify 'workbench.actions.view.problems', opts)
 
 -- Telescope: search/find related keymaps
 keymap('n', '<Leader>fs', notify 'workbench.action.findInFiles', opts) -- Fuzzy search
@@ -23,4 +30,5 @@ keymap('n', '<Leader>fc', notify 'workbench.action.showCommands', opts) -- find 
 keymap('n', '<Leader>ff', notify 'workbench.action.quickOpen', opts) -- find files
 
 -- UI
-keymap('n', '<Leader>fe', notify 'workbench.action.togglePanel', opts)
+keymap('n', '<Leader>fe', notify 'workbench.action.toggleSidebarVisibility', opts)
+keymap('n', '<Leader>tp', notify 'workbench.action.togglePanel', opts)
