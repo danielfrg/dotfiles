1677106264ef6e95682991c29924602d18c26aee
Daniel Rodriguez <daniel@danielfrg.com>
2023-05-02 00:39:37 -0500

    Neovim stuff

--- nvim/.config/nvim/lua/user/editors/vscode.lua
+++ nvim/.config/nvim/lua/user/editors/vscode.lua
@@ -14,13 +14,14 @@ local opts = { silent = true }
 keymap('n', '<leader>b', notify 'extension.goto-previous-buffer', opts)
 keymap('n', '<leader>`', notify 'extension.goto-previous-buffer', opts)
 keymap('n', '<S-x>', notify 'extension.goto-previous-buffer', opts)
+keymap('n', '<C-x>', notify 'extension.goto-previous-buffer', opts)
 
+-- LSP
+keymap('n', 'K', notify 'editor.action.showHover', opts)
 keymap('n', '<leader>la', notify 'editor.action.quickFix', opts)
 keymap('n', '<leader>lf', notify 'editor.action.formatDocument', opts)
 keymap('n', '<leader>lr', notify 'editor.action.rename', opts)
 
--- LSP related keymaps
-keymap('n', 'K', notify 'editor.action.showHover', opts)
 keymap('n', 'gd', notify 'editor.action.revealDefinition', opts)
 keymap('n', 'gD', notify 'editor.action.revealDeclaration', opts)
 keymap('n', 'gr', notify 'references-view.findReferences', opts)
@@ -30,9 +31,18 @@ keymap("n", "[d", notify 'editor.action.marker.next', opts)
 keymap("n", "]d", notify 'editor.action.marker.prev', opts)
 
 -- Telescope: search/find related keymaps
-keymap('n', '<leader>fg', notify 'workbench.action.findInFiles', opts) -- Fuzzy search
-keymap('n', '<leader>ff', notify 'workbench.action.quickOpen', opts) -- Find files
-keymap('n', '<leader>fc', notify 'workbench.action.showCommands', opts) -- Find commands
+keymap('n', '<leader>fg', notify 'workbench.action.findInFiles', opts)
+keymap('n', '<leader>ff', notify 'workbench.action.quickOpen', opts)
+keymap('n', '<leader>fc', notify 'workbench.action.showCommands', opts)
+
+-- Harpoon
+keymap('n', '<leader>a', notify 'vscode-harpoon.addEditor', opts)
+keymap('n', '<leader>h', notify 'vscode-harpoon.editorQuickPick', opts)
+keymap('n', '<C-h>', notify 'vscode-harpoon.gotoEditor1', opts)
+keymap('n', '<C-j>', notify 'vscode-harpoon.gotoEditor2', opts)
+keymap('n', '<C-k>', notify 'vscode-harpoon.gotoEditor3', opts)
+keymap('n', '<C-l>', notify 'vscode-harpoon.gotoEditor4', opts)
+
 
 -- Trouble / Problems
 keymap('n', '<leader>xd', notify 'workbench.actions.view.problems', opts)
