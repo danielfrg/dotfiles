f8827a85cf29c2e9fe072be861d8d81f401e26a2
Daniel Rodriguez <daniel@danielfrg.com>
2023-09-04 14:40:41 -0500

    NVChad JS config

---
+++ nvim/.config/nvim/lua/custom/configs/lspconfig.lua
@@ -0,0 +1,20 @@
+local config = require("plugins.configs.lspconfig")
+local on_attach = config.on_attach
+local capabilities = config.capabilities
+
+local lspconfig = require("lspconfig")
+
+lspconfig.tsserver.setup {
+    on_attach = on_attach,
+    capabilities = capabilities,
+    init_options = {
+        preferences = {
+            disableSuggestions = true,
+        }
+    }
+}
+
+-- Automatically run format on save
+vim.api.nvim_create_autocmd({ "BufWritePost" }, {
+    command = "FormatWriteLock"
+})
