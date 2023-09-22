2511c86cbd561889e49b48aa79fc4ea6ee9213d0
Daniel Rodriguez <daniel@danielfrg.com>
2023-09-21 23:56:27 -0500

    Add script for updating icons

---
+++ bin/.local/scripts/change-dock-icons.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+fileicon set /Applications/Visual\ Studio\ Code.app ~/Google\ Drive/My\ Drive/3\ Resources/1\ tech/mac/icons/gavin-nelson/vs-code/VS\ Code.icns
+
+fileicon set /Applications/Alacritty.app ~/Google\ Drive/My\ Drive/3\ Resources/1\ tech/mac/icons/terminal.icns
+
+fileicon set /Applications/Obsidian.app ~/Google\ Drive/My\ Drive/3\ Resources/1\ tech/mac/icons/gavin-nelson/obsidian/obsidian.icns
