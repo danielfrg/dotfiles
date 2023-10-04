89e10ddc9e25aeb2e795cba9463d6a1dce10cb82
Daniel Rodriguez <daniel@danielfrg.com>
2023-10-03 20:00:35 -0500

    Update

--- bin/.local/scripts/change-dock-icons.sh
+++ bin/.local/scripts/change-dock-icons.sh
@@ -2,7 +2,8 @@
 
 fileicon set /Applications/Alacritty.app ~/Google\ Drive/My\ Drive/3\ Resources/1\ tech/mac/icons/terminal.icns
 
-fileicon set /Applications/Brave\ Browser.app ~/Google\ Drive/My\ Drive/3\ Resources/1\ tech/mac/icons/brave.icns
+fileicon set /Applications/Chromium.app ~/Google\ Drive/My\ Drive/3\ Resources/1\ tech/mac/icons/chromium.icns
+# fileicon set /Applications/Brave\ Browser.app ~/Google\ Drive/My\ Drive/3\ Resources/1\ tech/mac/icons/brave.icns
 
 fileicon set /Applications/Obsidian.app ~/Google\ Drive/My\ Drive/3\ Resources/1\ tech/mac/icons/gavin-nelson/obsidian/obsidian.icns
 
