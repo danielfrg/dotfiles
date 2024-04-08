6a331b81e90e1c8384052b37c0f71f2f174f6a2e
Daniel Rodriguez <daniel@danielfrg.com>
2024-04-07 22:54:08 -0500

    colors

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,36 +1,36 @@
 return {
-  -- {
-  --   "catppuccin/nvim",
-  --   name = "catppuccin",
-  --   priority = 1000,
-  --   config = function()
-  --     vim.cmd([[colorscheme catppuccin-mocha]])
-  --   end,
-  -- },
-
   {
-    "rebelot/kanagawa.nvim",
+    "catppuccin/nvim",
+    name = "catppuccin",
     priority = 1000,
-
     config = function()
-      require('kanagawa').setup({
-        compile = true, -- enable compiling the colorscheme
+      vim.cmd([[colorscheme catppuccin-mocha]])
+    end,
+  },
 
-        theme = "wave", -- Load "wave" theme when 'background' option is not set
-        keywordStyle = { italic = true },
-
-        colors = {
-          theme = {
-            all = {
-              ui = {
-                bg_gutter = "none"
-              }
-            }
-          }
-        }
-      })
-
-      vim.cmd([[colorscheme kanagawa-wave]])
-    end
-  }
+  -- {
+  --   "rebelot/kanagawa.nvim",
+  --   priority = 1000,
+  --
+  --   config = function()
+  --     require('kanagawa').setup({
+  --       compile = true, -- enable compiling the colorscheme
+  --
+  --       theme = "wave", -- Load "wave" theme when 'background' option is not set
+  --       keywordStyle = { italic = true },
+  --
+  --       colors = {
+  --         theme = {
+  --           all = {
+  --             ui = {
+  --               bg_gutter = "none"
+  --             }
+  --           }
+  --         }
+  --       }
+  --     })
+  --
+  --     vim.cmd([[colorscheme kanagawa-wave]])
+  --   end
+  -- }
 }
