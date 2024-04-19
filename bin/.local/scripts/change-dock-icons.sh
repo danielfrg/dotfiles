98f3c4a6963a366a1ad7b7f236026330f9a401ef
Daniel Rodriguez <daniel@danielfrg.com>
2024-04-19 15:33:44 -0500

    update

--- bin/.local/scripts/change-dock-icons.sh
+++ bin/.local/scripts/change-dock-icons.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-fileicon set /Applications/WezTerm.app              /Users/danrodriguez/Library/Mobile\ Documents/com~apple~CloudDocs/Documents/3\ Resources/1\ tech/mac/icons/terminal.icns
-fileicon set /Applications/Visual\ Studio\ Code.app /Users/danrodriguez/Library/Mobile\ Documents/com~apple~CloudDocs/Documents/3\ Resources/1\ tech/mac/icons/vscode2.icns
+fileicon set /Applications/WezTerm.app              /Users/danrodriguez/Google\ Drive/My\ Drive/3\ Resources/1\ tech/mac/icons/terminal.icns
+fileicon set /Applications/Visual\ Studio\ Code.app /Users/danrodriguez/Google\ Drive/My\ Drive/3\ Resources/1\ tech/mac/icons/vscode2.icns
 
 #Resources fileicon set /Applications/Chromium.app ~/Google\ Drive/My\ Drive/3\ Resources/1\ tech/mac/icons/chromium.icns
 # fileicon set /Applications/Brave\ Browser.app ~/Google\ Drive/My\ Drive/3\ Resources/1\ tech/mac/icons/brave.icns
--- nvim/.config/nvim/lua/plugins/cmp.lua
+++ nvim/.config/nvim/lua/plugins/cmp.lua
@@ -1,142 +1,60 @@
 return {
-    {
-        "hrsh7th/nvim-cmp",
-        event = "VeryLazy",
-        -- event = "InsertEnter",
+  {
+    "L3MON4D3/LuaSnip",
+    dependencies = {
+      "saadparwaiz1/cmp_luasnip",
+      "rafamadriz/friendly-snippets",
+    },
+  },
 
-        dependencies = {
-            "saadparwaiz1/cmp_luasnip",
-            "hrsh7th/cmp-nvim-lsp",
-            "hrsh7th/cmp-path",
-            {
-                "zbirenbaum/copilot-cmp",
-                dependencies = "copilot.lua",
-                opts = {},
-                config = function(_, opts)
-                    local copilot_cmp = require("copilot_cmp")
-                    copilot_cmp.setup(opts)
-                    -- attach cmp source whenever copilot attaches
-                    -- fixes lazy-loading issues with the copilot cmp source
-                    -- LazyVim.lsp.on_attach(function(client)
-                    --     if client.name == "copilot" then
-                    --         copilot_cmp._on_insert_enter({})
-                    --     end
-                    -- end)
-                end,
-            },
-        },
+  {
+    "hrsh7th/cmp-path",
+    event = "VeryLazy",
+  },
 
-        config = function(_, opts)
-            local cmp = require('cmp')
-            local luasnip = require('luasnip')
-            luasnip.config.setup {}
+  {
+    "hrsh7th/cmp-nvim-lsp",
+    event = "VeryLazy",
+  },
 
-            cmp.setup({
-                snippet = {
-                    expand = function(args)
-                        luasnip.lsp_expand(args.body)
-                    end,
-                },
-                completion = { completeopt = 'menu,menuone,noinsert' },
-                window = {
-                    completion = cmp.config.window.bordered(),
-                    documentation = cmp.config.window.bordered(),
-                },
-                mapping = cmp.mapping.preset.insert({
-                    -- Select the [n]ext item
-                    ['<C-n>'] = cmp.mapping.select_next_item(),
-                    -- Select the [p]revious item
-                    ['<C-p>'] = cmp.mapping.select_prev_item(),
-
-                    -- Scroll the documentation window [b]ack / [f]orward
-                    ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-                    ['<C-f>'] = cmp.mapping.scroll_docs(4),
-
-                    -- Accept ([y]es) the completion.
-                    --  This will auto-import if your LSP supports it.
-                    --  This will expand snippets if the LSP sent a snippet.
-                    ['<C-y>'] = cmp.mapping.confirm { select = true },
-                    ["<CR>"] = cmp.mapping.confirm({ select = true }),
-
-                    -- Manually trigger a completion from nvim-cmp.
-                    --  Generally you don't need this, because nvim-cmp will display
-                    --  completions whenever it has completion options available.
-                    ['<M-Space>'] = cmp.mapping.complete {},
-
-                    -- Think of <c-l> as moving to the right of your snippet expansion.
-                    --  So if you have a snippet that's like:
-                    --  function $name($args)
-                    --    $body
-                    --  end
-                    --
-                    -- <c-l> will move you to the right of each of the expansion locations.
-                    -- <c-h> is similar, except moving you backwards.
-                    ['<C-l>'] = cmp.mapping(function()
-                        if luasnip.expand_or_locally_jumpable() then
-                            luasnip.expand_or_jump()
-                        end
-                    end, { 'i', 's' }),
-                    ['<C-h>'] = cmp.mapping(function()
-                        if luasnip.locally_jumpable(-1) then
-                            luasnip.jump(-1)
-                        end
-                    end, { 'i', 's' }),
-                    -- Trying to not use <Tab> and <S-Tab>
-                    -- ["<Tab>"] = cmp.mapping(function(fallback)
-                    --     if cmp.visible() then
-                    --         cmp.select_next_item()
-                    --     elseif require("luasnip").expand_or_jumpable() then
-                    --         vim.fn.feedkeys(
-                    --             vim.api.nvim_replace_termcodes("<Plug>luasnip-expand-or-jump", true, true, true), "")
-                    --     else
-                    --         fallback()
-                    --     end
-                    -- end, {
-                    --     "i",
-                    --     "s",
-                    -- }),
-                    -- ["<S-Tab>"] = cmp.mapping(function(fallback)
-                    --     if cmp.visible() then
-                    --         cmp.select_prev_item()
-                    --     elseif require("luasnip").jumpable(-1) then
-                    --         vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Plug>luasnip-jump-prev", true, true, true),
-                    --             "")
-                    --     else
-                    --         fallback()
-                    --     end
-                    -- end, {
-                    --     "i",
-                    --     "s",
-                    -- }),
-                }),
-                sources = cmp.config.sources({
-                    { name = 'nvim_lsp' },
-                    { name = 'luasnip' },
-                    { name = 'path' },
-                    { name = 'copilot' },
-                })
-            })
-        end,
-
+  {
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+      "hrsh7th/cmp-path",
+      "hrsh7th/cmp-nvim-lsp",
+      "nvim-lspconfig",
+      "zbirenbaum/copilot-cmp",
+      "saadparwaiz1/cmp_luasnip",
     },
 
-    {
-        "L3MON4D3/LuaSnip",
-        lazy = true,
-        dependencies = {
-            "saadparwaiz1/cmp_luasnip",
-            {
-                "rafamadriz/friendly-snippets",
-                config = function()
-                    require("luasnip.loaders.from_vscode").lazy_load()
-                end,
-            },
+    config = function()
+      local cmp = require("cmp")
+      require("luasnip.loaders.from_vscode").lazy_load()
+
+      cmp.setup({
+        snippet = {
+          expand = function(args)
+            require("luasnip").lsp_expand(args.body)
+          end,
         },
-        build = (function()
-            -- Build Step is needed for regex support in snippets.
-            return "make install_jsregexp"
-        end)(),
-    },
-
-
+        window = {
+          completion = cmp.config.window.bordered(),
+          documentation = cmp.config.window.bordered(),
+        },
+        mapping = cmp.mapping.preset.insert({
+          ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+          ["<C-f>"] = cmp.mapping.scroll_docs(4),
+          ["<C-Space>"] = cmp.mapping.complete(),
+          ["<C-e>"] = cmp.mapping.abort(),
+          ["<CR>"] = cmp.mapping.confirm({ select = true }),
+        }),
+        sources = cmp.config.sources({
+          { name = "nvim_lsp" },
+          { name = 'path' },
+          { name = "luasnip" },
+        }, {
+        }),
+      })
+    end,
+  },
 }
