1e64295260a8fe85fdf1bad0cb89b77ee847f1ef
Daniel Rodriguez <daniel@danielfrg.com>
2024-04-25 17:58:17 -0500

    update

--- nvim/.config/nvim/lua/autocmds.lua
+++ nvim/.config/nvim/lua/autocmds.lua
@@ -10,16 +10,16 @@ vim.api.nvim_create_autocmd('TextYankPost', {
 })
 
 -- Open the dashboard on the last buffer
-vim.api.nvim_create_augroup("dashboard_on_empty", { clear = true })
-vim.api.nvim_create_autocmd("User", {
-    pattern = "BDeletePre *",
-    group = "dashboard_on_empty",
-    callback = function()
-        local bufnr = vim.api.nvim_get_current_buf()
-        local name = vim.api.nvim_buf_get_name(bufnr)
+-- vim.api.nvim_create_augroup("dashboard_on_empty", { clear = true })
+-- vim.api.nvim_create_autocmd("User", {
+--     pattern = "BDeletePre *",
+--     group = "dashboard_on_empty",
+--     callback = function()
+--         local bufnr = vim.api.nvim_get_current_buf()
+--         local name = vim.api.nvim_buf_get_name(bufnr)
 
-        if name == "" then
-            vim.cmd([[:Dashboard]])
-        end
-    end,
-})
+--         if name == "" then
+--             vim.cmd([[:Dashboard]])
+--         end
+--     end,
+-- })
--- nvim/.config/nvim/lua/plugins/mason.lua
+++ nvim/.config/nvim/lua/plugins/mason.lua
@@ -1,52 +1,50 @@
-return {}
--- return {
---     {
---         "williamboman/mason.nvim",
---         -- event = "VeryLazy",
---         cmd = { "Mason", "MasonInstall", "MasonUpdate" },
---         keys = { { "<leader>cm", "<cmd>Mason<cr>", desc = "Mason" } },
---         build = ":MasonUpdate",
+return {
+    {
+        "williamboman/mason.nvim",
+        -- event = "VeryLazy",
+        cmd = { "Mason", "MasonInstall", "MasonUpdate" },
+        keys = { { "<leader>cm", "<cmd>Mason<cr>", desc = "Mason" } },
+        build = ":MasonUpdate",
 
---         config = function()
---             require("mason").setup()
---         end
---     },
---     {
---         "williamboman/mason-lspconfig.nvim",
---         event = "VeryLazy",
---         dependencies = { "mason.nvim" },
+        config = function()
+            require("mason").setup()
+        end
+    },
+    {
+        "williamboman/mason-lspconfig.nvim",
+        event = "VeryLazy",
+        dependencies = { "mason.nvim" },
 
---         opts = function()
---             return {
---                 -- https://github.com/williamboman/mason-lspconfig.nvim/blob/main/doc/server-mapping.md
---                 ensure_installed = {
---                     -- Python
---                     "pyright",
---                     -- "pylsp",
---                     "ruff_lsp",
+        opts = function()
+            return {
+                -- https://github.com/williamboman/mason-lspconfig.nvim/blob/main/doc/server-mapping.md
+                ensure_installed = {
+                    -- Python
+                    "pyright",
+                    -- "pylsp",
+                    "ruff_lsp",
 
---                     -- Web
---                     "astro",
---                     "cssls",
---                     "eslint",
---                     "html",
---                     "tailwindcss",
---                     "tsserver",
---                     "svelte",
+                    -- Web
+                    "astro",
+                    "cssls",
+                    "eslint",
+                    "html",
+                    "tailwindcss",
+                    "tsserver",
+                    "svelte",
 
---                     -- Other
---                     "terraformls",
---                     "ansiblels",
+                    -- Other
+                    "terraformls",
+                    "ansiblels",
 
---                     -- Lua
---                     "lua_ls",
---                 },
---             }
---         end,
+                    -- Lua
+                    "lua_ls",
+                },
+            }
+        end,
 
---         config = function(_, opts)
---             require("mason-lspconfig").setup(opts)
---         end
---     }
-
--- }
+        config = function(_, opts)
+            require("mason-lspconfig").setup(opts)
+        end
+    }
+}
