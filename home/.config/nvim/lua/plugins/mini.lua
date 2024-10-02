d87fd1e440d075421e82fc5054ce949d6e7326e1
Daniel Rodriguez <daniel@danielfrg.com>
2024-10-02 10:07:03 -0500

    cleanup

--- home/.config/nvim/lua/plugins/mini.lua
+++ home/.config/nvim/lua/plugins/mini.lua
@@ -3,10 +3,17 @@ return {
         'echasnovski/mini.nvim',
         version = '*',
         config = function()
-            require('mini.align').setup()
             require('mini.pairs').setup()
+
+            -- Add/delete/replace surroundings (brackets, quotes, etc.)
+            --
+            -- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
+            -- - sd"   - [S]urround [D]elete ["]quotes
+            -- - sr)"  - [S]urround [R]eplace [)] ["]
             require('mini.surround').setup()
+
             require('mini.bracketed').setup()
+            require("mini.comment").setup()
         end,
     },
 }
