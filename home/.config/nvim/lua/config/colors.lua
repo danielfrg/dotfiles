1f38d5ad4a7fd8411bbbfdb6b395c0e09bf9bced
Daniel Rodriguez <daniel@danielfrg.com>
2024-10-16 17:41:22 -0500

    colors

--- home/.config/nvim/lua/config/colors.lua
+++ home/.config/nvim/lua/config/colors.lua
@@ -4,18 +4,20 @@ local set_hl = vim.api.nvim_set_hl
 vim.cmd("colorscheme default")
 
 palette = {
-    black = "#16181D",
+    bg = "#16181D",
     red = "#f08080",
     green = "#a1c181",
     yellow = "#fce094",
     orange = "#ffa07a",
     blue = "#73ADE9",
 
+    black = "#000000",
     gray1 = "#5D636F",
+    gray2 = "#21252B",
     white0 = "#eef1f8",
 }
 
-set_hl(0, "Normal", { bg = palette.black, fg = palette.white0 })
+set_hl(0, "Normal", { bg = palette.bg, fg = palette.white0 })
 set_hl(0, "Identifier", { fg = palette.white0 })
 set_hl(0, "Keyword", { fg = palette.orange })
 set_hl(0, "Boolean", { fg = palette.yellow })
@@ -34,8 +36,22 @@ set_hl(0, "@tag.builtin.tsx", { fg = palette.white0 })
 set_hl(0, "@tag.delimiter.tsx", { fg = palette.white0 })
 set_hl(0, "@tag.attribute.tsx", { fg = palette.white0 })
 
--- function set_highlights_table(table)
---     for group, config in pairs(table) do
---         vim.api.nvim_set_hl(0, group, config)
---     end
--- end
+set_hl(0, "StatusLine",  { fg="#505259", bg=palette.black })
+
+-- Telescope.
+set_hl(0, "TelescopePromptPrefix",  { fg=palette.yellow, bg=palette.gray2 })
+set_hl(0, "TelescopeTitle",  { fg=palette.bg, bg=palette.orange })
+set_hl(0, "TelescopeNormal", { bg = palette.gray2 })
+
+function set_highlights_table(table)
+    for group, config in pairs(table) do
+        vim.api.nvim_set_hl(0, group, config)
+    end
+end
+
+set_highlights_table({
+    -- Git
+    Added = { fg = palette.green },
+    Removed = { fg = palette.red },
+    Changed = { fg = palette.blue },
+})
