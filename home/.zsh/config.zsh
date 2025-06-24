e89d06eda3429d5feeb53871ec6d0ebaca3507ac
Daniel Rodriguez <daniel@danielfrg.com>
2025-06-23 19:40:55 -0500

    update

--- home/.zsh/config.zsh
+++ home/.zsh/config.zsh
@@ -186,7 +186,7 @@ alias lt='ls --tree'
 if [[ $(uname) == "Darwin" ]]; then
   alias md5sum='md5 -r'
   alias sha256sum="shasum -a 256"
-  alias rm="/opt/homebrew/opt/trash-cli/bin/trash"
+  # alias rm="/opt/homebrew/opt/trash-cli/bin/trash"
 fi
 
 # git
